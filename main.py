ce7c550e31f2457778d50c5e3a17fb91fdde681d
AlboMonco <albomonaco@gmail.com>
2021-11-18 17:07:33 -0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from typing_extensions import ParamSpecArgs
 from fastapi import FastAPI, datastructures
 from typing import Optional
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -24,9 +26,22 @@ def unpublished():
 def comments(id,limit:int):
     return {"data": {"1","2", limit}}
 
-
 class Blog(BaseModel):
     title: str
     body: str
-    published: Optional[bool]
+    published: bool
 
+
+@app.post("/myblogs")
+def create_blog(request:Blog):
+    if request.published:
+        return {'data':{'title':request.title,'body': request.body,'published': "Your blog was published"}}
+    else:
+        return {'data':{'title':request.title,'body': request.body,'published': "Your blog was not published"}}
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
