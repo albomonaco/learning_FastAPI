54c7d2e2a14ebad2bb9fbf7ae577230b51db0b23
AlboMonco <albomonaco@gmail.com>
2021-11-25 09:18:27 -0300

    main

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-import schemas
-import models
-from database import engine
+from . import models, schemas
+from .database import engine
+from sqlalchemy import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create(request:schemas.Blog):
-    return request
+@app.post('/al')
+def create(request:schemas.Blog, db:Session):
+    return 1
